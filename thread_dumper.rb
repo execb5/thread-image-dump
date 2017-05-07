9f526acd6efcfb82d191884fa5bb9133a10e115d
Matthias Nunes <matthiasnunes@gmail.com>
2017-05-07 17:50:48 -0300

    retabs

--- thread_dumper.rb
+++ thread_dumper.rb
@@ -15,23 +15,23 @@ FileUtils.mkdir_p local_folder
 
 images = page.css('.fileText a')
 images.each do |img|
-	fnum = img['href'].split('/').last.split('.').first
-	local_fname = "#{DUMP_FOLDER}/#{subject}/#{fnum}_#{img.text}"
-	remote_url = "http:#{img['href']}"
-	unless File.exists? local_fname
-		puts "Fetching #{remote_url}..."
-		begin
-			img_content = open(remote_url).read
-		rescue Exception=>e
-			puts "Error: #{e}"
-			sleep 5
-		else
-			File.open(local_fname, 'wb'){|file| file.write(img_content)}
-			puts "\t...Success, saved to #{local_fname}"
-		ensure
-			sleep 1.0 + rand
-		end
-	end
+  fnum = img['href'].split('/').last.split('.').first
+  local_fname = "#{DUMP_FOLDER}/#{subject}/#{fnum}_#{img.text}"
+  remote_url = "http:#{img['href']}"
+  unless File.exists? local_fname
+    puts "Fetching #{remote_url}..."
+    begin
+      img_content = open(remote_url).read
+    rescue Exception=>e
+      puts "Error: #{e}"
+      sleep 5
+    else
+      File.open(local_fname, 'wb'){|file| file.write(img_content)}
+      puts "\t...Success, saved to #{local_fname}"
+    ensure
+      sleep 1.0 + rand
+    end
+  end
 end
 
 
