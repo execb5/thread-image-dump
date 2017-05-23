6eb2fa3a3bc779c33f47af3f5adfe77c86e103b0
Matthias Nunes <matthiasnunes@gmail.com>
2017-05-22 22:02:54 -0300

    Adds gemfile with dependecies and executes stuff in parallel

--- thread_dumper.rb
+++ thread_dumper.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 require 'fileutils'
+require 'parallel'
 
 DUMP_FOLDER = "dump"
 
@@ -14,24 +15,17 @@ local_folder = "#{DUMP_FOLDER}/#{subject}"
 FileUtils.mkdir_p local_folder
 
 images = page.css('.fileText a')
-images.each do |img|
+Parallel.each(images, in_processes: 8, progress: "Downloading... #{subject} thread") do |img|
   fnum = img['href'].split('/').last.split('.').first
   local_fname = "#{DUMP_FOLDER}/#{subject}/#{fnum}_#{img.text}"
   remote_url = "http:#{img['href']}"
   unless File.exists? local_fname
-    puts "Fetching #{remote_url}..."
     begin
       img_content = open(remote_url).read
     rescue Exception=>e
       puts "Error: #{e}"
-      sleep 5
     else
       File.open(local_fname, 'wb'){|file| file.write(img_content)}
-      puts "\t...Success, saved to #{local_fname}"
-    ensure
-      sleep 1.0 + rand
     end
   end
 end
-
-
