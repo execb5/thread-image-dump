5fbbd7a1c89c24355cc9794d1bab8189f8d62baf
Jiéverson Maissiat <jieverson@gmail.com>
2017-05-23 02:31:50 +0000

    add node script

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # thread-image-dump
 
-A simple ruby script to dump all images of a 4chan thread in a new folder on the
+A simple script to dump all images of a 4chan thread in a new folder on the
 current working directory.
 
-Usage:
+Ruby Usage:
 ```sh
 bundle install
 ruby thread_dumper.rb <THREAD_LINK>
+```
+
+Node Usage:
+```sh
+npm install
+node thread_dumper.js <THREAD_LINK>
 ```
---
+++ thread_dumper.js
@@ -0,0 +1,50 @@
+const fetch = require('node-fetch')
+const cheerio = require('cheerio')
+const fs = require('fs')
+const term = require('terminal-kit').terminal
+
+const DUMP_FOLDER = "dump"
+
+const thread_url = process.argv[2]
+//const thread_url = 'http://boards.4chan.org/w/thread/2012093'
+
+progressBar = term.progressBar({
+	width: 80,
+	title: 'dumping:',
+	eta: true,
+	percent: true
+})
+
+fetch(thread_url)
+    .then(response => response.text())
+    .then(body => {
+        let $ = cheerio.load(body)
+
+        let subject = $('.subject')[1].children[0].data.replace(/[^\w\s]/gi, '')
+        let dir = DUMP_FOLDER + '/' + subject 
+
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+        }
+
+        let progress = 0
+
+        let images = $('.fileText a')
+        for(let i = 0; i < images.length; i++){
+            let img = images[i]
+            let href = 'http:' + img.attribs['href']
+            let file_name = href.split('/').reverse()[0]
+            let local_name = file_name.split('.')[0]
+                + img.next.data + '.'
+                + file_name.split('.')[1]
+            fetch(href)
+                .then(res => {
+                    let dest = fs.createWriteStream(
+                        './' + dir + '/' + local_name)
+                    res.body.pipe(dest).on('finish', () => {
+                        progress += (1 / images.length) + 0.01
+                        progressBar.update(progress)
+                    })
+                })
+        }
+    })
